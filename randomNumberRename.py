fc4e3bca9e156cbb21240ee2ea480a52e3bc1121
unknown <arminhahn@gmx.de>
2019-06-03 15:09:35 +0200

    add rename with colors

--- randomNumberRename.py
+++ randomNumberRename.py
@@ -20,5 +20,20 @@ def write_log_file(outputfilename, data):
 
 if __name__ == "__main__":
     outputfilename = "randomNumber2"
-    roll = get_random_number(1, 100)
+    roll = get_random_number(1, 6)
     write_log_file(outputfilename, roll)
+
+def get_color_by_dice_naive(spots):
+    if spots == 1:
+        color = 'blue'
+    elif spots == 2:
+        color = 'green'
+    elif spots == 3:
+        color = 'red'
+    elif spots == 4:
+        color = 'yellow'
+    elif spots == 5:
+        color = 'purple'
+    else: # spots == 6:
+        color = 'orange'
+    return color
